6562a0ffe60bc6f7611e3e42377e4948ca5b9a25
cngondo <devngondo@gmail.com>
2017-01-10 00:10:47 +0300

    all tests pass: successful build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-
-    System.out.println("I'm a ping-pong application!");
-
+    //Prompt user input
+    System.out.println("Enter a number: ");
+    //Capture user input
+    Scanner input = new Scanner(System.in);
+    int inputNumber = input.nextInt();
+    //output the result in a list object
+    PingPong pingPong = new PingPong();
+    List<Object> pingpongResult = pingPong.runPingPong(inputNumber);   
+    System.out.println(pingpongResult);
   }
 }
