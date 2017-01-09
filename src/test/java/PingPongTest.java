fa42ef120d6f89de921e8efa658a536c24562c1e
cngondo <devngondo@gmail.com>
2017-01-09 23:19:24 +0300

    setting up gradle, test fails

---
+++ src/test/java/PingPongTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PingPongTest {
+
+  @Test
+  public void runPingPong_countUpToOne_ArrayList() {
+  	PingPong testPingPong = new PingPong();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    assertEquals(expectedOutput, testPingPong.runPingPong(1));
+  }
+
+}
